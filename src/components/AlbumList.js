8295048b4ebe22fb9a1bac19b03d7386888a6489
Christopher McIntosh <chrismac92@gmail.com>
2018-06-13 13:01:24 -0400

    update album detail and album list

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import axios from 'axios';
 import AlbumDetail from './AlbumDetail';
+
 class AlbumList extends Component {
   constructor() {
     super();
@@ -18,6 +19,7 @@ class AlbumList extends Component {
   }
 
   renderAlbums() {
+    console.log(this.state.albums);
     return this.state.albums.map(album => (
       <AlbumDetail key={album.title} album={album} />
     ));
