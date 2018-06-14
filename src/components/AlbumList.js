603fdd7077c05e50b56257166b8c3819237e56cc
Christopher McIntosh <chrismac92@gmail.com>
2018-06-14 05:15:58 -0400

    added button

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { ScrollView } from 'react-native';
 import axios from 'axios';
 import AlbumDetail from './AlbumDetail';
 
@@ -19,14 +19,13 @@ class AlbumList extends Component {
   }
 
   renderAlbums() {
-    console.log(this.state.albums);
     return this.state.albums.map(album => (
       <AlbumDetail key={album.title} album={album} />
     ));
   }
 
   render() {
-    return <View>{this.renderAlbums()}</View>;
+    return <ScrollView>{this.renderAlbums()}</ScrollView>;
   }
 }
 
